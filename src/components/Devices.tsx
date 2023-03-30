046a894685705b876e653695894c515ebd3fb0f3
Amanda Fernandes <amanda.fernandes@tvnsports.com.br>
2023-03-30 18:39:36 -0300

    feat: update devices

--- src/components/Devices.tsx
+++ src/components/Devices.tsx
@@ -9,25 +9,25 @@ const devices = [
   },
   {
     id: 2,
-    name: 'iOS Mobile',
-    image:
-      'https://static.wixstatic.com/media/315203_c72315e16953492aa69b3cc5ead534f5~mv2.png/v1/fill/w_288,h_288,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/VBtv-Icons_03.png',
-  },
-  {
-    id: 3,
-    name: 'Android Mobile',
-    image:
-      'https://static.wixstatic.com/media/315203_b12ec0944f874e019d726babecc7db80~mv2.png/v1/fill/w_294,h_294,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/VBtv-Icons_04.png',
-  },
-  {
-    id: 4,
     name: 'Chromecast',
     image:
       'https://static.wixstatic.com/media/315203_b12ec0944f874e019d726babecc7db80~mv2.png/v1/fill/w_294,h_294,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/VBtv-Icons_04.png',
   },
   {
-    id: 5,
+    id: 3,
     name: 'Web',
+    image:
+      'https://static.wixstatic.com/media/315203_8709e523dedc437986de58e5712b2756~mv2.png/v1/fill/w_300,h_288,al_c,q_85,enc_auto/VBtv-Icons_01.png',
+  },
+  {
+    id: 4,
+    name: 'iOS Mobile',
+    image:
+      'https://static.wixstatic.com/media/315203_c72315e16953492aa69b3cc5ead534f5~mv2.png/v1/fill/w_288,h_288,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/VBtv-Icons_03.png',
+  },
+  {
+    id: 5,
+    name: 'Android Mobile',
     image:
       'https://static.wixstatic.com/media/315203_8709e523dedc437986de58e5712b2756~mv2.png/v1/fill/w_300,h_288,al_c,q_85,enc_auto/VBtv-Icons_01.png',
   },
@@ -38,25 +38,25 @@ export function Devices() {
     <section className="relative flex flex-col items-center py-24 px-4 sm:px-6 md:px-8 lg:px-10">
       <div className="mx-auto max-w-4xl text-center">
         <h2 className="mt-2 text-4xl font-bold tracking-tight text-white sm:text-5xl">
-          LOREM. IPSUM. LOREM IPSUM
+          Assista como e de onde quiser!
         </h2>
       </div>
-      <p className="mx-auto mt-6 mb-10 text-center text-2xl leading-8 text-gray-300">
-        Disponível em todos os dispositivos: Apple TV, iOS Mobile, Android
-        Mobile, Chromecast e Web
+      <p className="mx-auto mt-6 mb-10 text-center text-2xl leading-8 text-gray-300 max-w-[60ch]">
+        Espelhe para a tela grande da TV ou assista no tablet, laptop, celular e
+        outros aparelhos. Transmissões em Full HD.
       </p>
 
       <button className="py-3 bg-medium-blue font-bold text-lg w-60 rounded mb-10">
         Assine agora
       </button>
 
-      <div className="flex flex-col justify-between gap-4 mx-auto sm:flex-row sm:flex-wrap lg:grid lg:grid-cols-5">
+      <div className="w-full flex-1 flex flex-wrap justify-center gap-2 mx-auto sm:flex-row lg:max-w-6xl lg:gap-4 lg:grid lg:grid-cols-5">
         {devices.map((device) => (
           <div
             key={device.id}
             className="flex flex-col justify-center items-center border border-[rgb(38,75,114)] rounded-md px-8 py-6 backdrop-blur-xl bg-transparent"
           >
-            <span className="text-white">{device.name}</span>
+            <span className="text-white text-center">{device.name}</span>
             <Image
               src={device.image}
               alt=""
