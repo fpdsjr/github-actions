50c805f81a67d930b1b4d9d278c50b5dbd386ce2
Amanda Fernandes <amanda.fernandes@tvnsports.com.br>
2023-04-04 13:58:33 -0300

    feat: get token

--- src/pages/[provider]/[token].tsx
+++ src/pages/[provider]/[token].tsx
@@ -1,10 +1,14 @@
 import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
-import Home from '@/pages'
+import Home from '@/pages/home'
 
 export default function Token() {
+  const router = useRouter()
+  console.log(router.query.ct)
+
   useEffect(() => {
-    window.history.replaceState({}, '', '/')
+    // window.history.replaceState({}, '', '/')
   })
 
   return <Home />
