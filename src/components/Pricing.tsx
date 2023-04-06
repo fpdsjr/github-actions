a529724440dc04a621427cf2f2de9545849bf1c0
Amanda Fernandes <amanda.fernandes@tvnsports.com.br>
2023-04-06 15:51:14 -0300

    feat: remove single plan

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -14,15 +14,6 @@ const setShortToken = (token: string, idx: number) => {
 
 const tiers = [
   {
-    id: 'monthly',
-    name: 'Novo Canal Vôlei Brasil',
-    description: 'MENSAL - cancele quando quiser',
-    href: `https://canalvoleibrasil.cbv.com.br/user/token?ct=&redirect=/plans?event_id=13502&plan=105`,
-    price: 'R$ 12,90',
-    mostPopular: false,
-    textButton: 'Assinar mensal',
-  },
-  {
     id: 'yearly',
     name: 'Novo Canal Vôlei Brasil',
     description: 'ANUAL - em até 12x sem juros',
@@ -35,14 +26,23 @@ const tiers = [
     textButton: 'Assinar anual',
   },
   {
-    id: 'single',
+    id: 'monthly',
     name: 'Novo Canal Vôlei Brasil',
-    description: 'JOGO AVULSO - somente o jogo selecionado',
-    href: 'https://canalvoleibrasil.cbv.com.br/user/token?ct=&redirect=/plans?event_id=13502',
-    price: 'R$ 22,90',
+    description: 'MENSAL - cancele quando quiser',
+    href: `https://canalvoleibrasil.cbv.com.br/user/token?ct=&redirect=/plans?event_id=13502&plan=105`,
+    price: 'R$ 12,90',
     mostPopular: false,
-    textButton: 'Comprar 1 jogo',
+    textButton: 'Assinar mensal',
   },
+  // {
+  //   id: 'single',
+  //   name: 'Novo Canal Vôlei Brasil',
+  //   description: 'JOGO AVULSO - somente o jogo selecionado',
+  //   href: 'https://canalvoleibrasil.cbv.com.br/user/token?ct=&redirect=/plans?event_id=13502',
+  //   price: 'R$ 22,90',
+  //   mostPopular: false,
+  //   textButton: 'Comprar 1 jogo',
+  // },
 ]
 
 export function Pricing() {
@@ -56,23 +56,23 @@ export function Pricing() {
 
   return (
     <div className="relative py-24 bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-blue-900 via-dark-blue to-dark-blue sm:py-32">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+      <div className="mx-auto px-6 flex flex-col lg:px-8">
         <div className="mx-auto max-w-4xl text-center">
           <h2 className="mt-2 text-4xl font-bold tracking-tight text-white sm:text-5xl">
             O vôlei brasileiro com você o ano inteiro
           </h2>
+          <p className="mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-300">
+            Escolha o tipo de acesso ideal para você
+          </p>
         </div>
-        <p className="mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-300">
-          Escolha o tipo de acesso ideal para você
-        </p>
 
-        <div className="relative mx-auto mt-10 grid max-w-md grid-cols-1 gap-8 lg:mx-auto lg:max-w-5xl lg:grid-cols-3">
+        <div className="mx-auto mt-10 grid max-w-md grid-cols-1 gap-8 lg:mx-auto lg:max-w-5xl lg:grid-cols-2">
           {tiers.map((tier, idx) => (
             <div
               key={tier.id}
               className={classNames(
                 tier.mostPopular ? 'bg-white/5 ring-2 ring-medium-blue' : '',
-                'rounded-lg py-8 px-6 flex flex-col justify-between items-center bg-gradient-to-b from-[rgba(255,255,255,0.12)0%] to-[rgba(255,255,255,0)100%]',
+                'relative rounded-lg py-8 px-6 flex flex-col justify-between items-center bg-gradient-to-b from-[rgba(255,255,255,0.12)0%] to-[rgba(255,255,255,0)100%]',
               )}
             >
               <div className="flex flex-col items-center justify-between gap-x-4">
@@ -87,7 +87,7 @@ export function Pricing() {
                   <li className="mt-4 font-medium">{tier.tag}</li>
                 </ul>
                 {tier.mostPopular ? (
-                  <p className="rounded-full absolute left-1/2 -translate-x-1/2 -top-4 bg-medium-blue py-1 px-4 uppercase font-bold leading-5 text-white">
+                  <p className="rounded-full absolute left-1/2 -translate-x-1/2 -top-4 whitespace-nowrap bg-medium-blue py-1 px-4 uppercase font-bold leading-5 text-white">
                     Mais popular
                   </p>
                 ) : null}
