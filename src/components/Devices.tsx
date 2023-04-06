dfc1cdbe860ffc3e2d3400f7e17a76909f4e2545
Amanda Fernandes <amanda.fernandes@tvnsports.com.br>
2023-04-05 23:34:01 -0300

    feat: update assets and add devices

--- src/components/Devices.tsx
+++ src/components/Devices.tsx
@@ -3,36 +3,31 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { useContext } from 'react'
 
+import Apps from '@/assets/devices/apps.png'
+import Tv from '@/assets/devices/tv.png'
+import Chromecast from '@/assets/devices/chromecast.png'
+import Web from '@/assets/devices/web.png'
+
 const devices = [
   {
     id: 1,
-    name: 'Apple TV',
-    image:
-      'https://static.wixstatic.com/media/315203_8709e523dedc437986de58e5712b2756~mv2.png/v1/fill/w_300,h_288,al_c,q_85,enc_auto/VBtv-Icons_01.png',
+    name: 'Apps IOS e Android',
+    image: Apps,
   },
   {
     id: 2,
-    name: 'Chromecast',
-    image:
-      'https://static.wixstatic.com/media/315203_b12ec0944f874e019d726babecc7db80~mv2.png/v1/fill/w_294,h_294,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/VBtv-Icons_04.png',
+    name: 'Chromecast e Air Play',
+    image: Chromecast,
   },
   {
     id: 3,
-    name: 'Web',
-    image:
-      'https://static.wixstatic.com/media/315203_8709e523dedc437986de58e5712b2756~mv2.png/v1/fill/w_300,h_288,al_c,q_85,enc_auto/VBtv-Icons_01.png',
+    name: 'TVs AndroidTV e Samsung (em breve)',
+    image: Tv,
   },
   {
     id: 4,
-    name: 'iOS Mobile',
-    image:
-      'https://static.wixstatic.com/media/315203_c72315e16953492aa69b3cc5ead534f5~mv2.png/v1/fill/w_288,h_288,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/VBtv-Icons_03.png',
-  },
-  {
-    id: 5,
-    name: 'Android Mobile',
-    image:
-      'https://static.wixstatic.com/media/315203_8709e523dedc437986de58e5712b2756~mv2.png/v1/fill/w_300,h_288,al_c,q_85,enc_auto/VBtv-Icons_01.png',
+    name: 'Chrome, Edge, Safari e outros',
+    image: Web,
   },
 ]
 
@@ -72,20 +67,20 @@ export function Devices() {
         </button>
       )}
 
-      <div className="w-full flex-1 flex flex-wrap justify-center gap-2 mx-auto sm:flex-row lg:max-w-6xl lg:gap-4 lg:grid lg:grid-cols-5">
+      <div className="w-full grid grid-cols-2 gap-2 mx-auto md:grid-cols-4 lg:max-w-4xl lg:gap-4">
         {devices.map((device) => (
           <div
             key={device.id}
-            className="flex flex-col justify-center items-center border-2 border-[rgb(38,75,114)] rounded-lg px-6 py-4 backdrop-blur-xl bg-transparent"
+            className="grid grid-rows-2 place-items-center gap-1 border-2 border-[rgb(38,75,114)] rounded-lg px-6 py-4 backdrop-blur-xl bg-transparent"
           >
-            <span className="text-white text-center">{device.name}</span>
             <Image
               src={device.image}
               alt=""
-              width={100}
-              height={100}
-              className="invert"
+              width={60}
+              height={60}
+              className="place-self-center"
             />
+            <span className="text-white text-center">{device.name}</span>
           </div>
         ))}
       </div>
